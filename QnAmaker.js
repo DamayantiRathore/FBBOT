1fc9d6e8f46ed59ca2fdbf0f68085d0bde73c8cd
cts <cts@PC267975.local>
2018-02-09 15:30:27 +0530

    First Project commited

---
+++ QnAmaker.js
@@ -0,0 +1,37 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+var cognitiveservices = require('botbuilder-cognitiveservices');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+ appId: process.env.MICROSOFT_APP_ID,
+ appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var recognizer = new cognitiveservices.QnAMakerRecognizer({
+	knowledgeBaseId: '4ab60278-7c54-4bb2-8a1a-cf9d9109ac41',
+	subscriptionKey: '6511e59f6d1b4baaacb6c4337abd5882'});
+
+var basicQnAMakerDialog = new cognitiveservices.QnAMakerDialog({
+	recognizers: [recognizer],
+	defaultMessage: 'No match! Try changing the query terms!',
+	qnaThreshold: 0.3
+});
+
+bot.dialog('/', basicQnAMakerDialog);
