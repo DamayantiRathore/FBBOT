bd021d30b84cdc2ccff3b8c7c5f9c979d7ccb312
cts <cts@PC267975.local>
2018-02-19 19:25:01 +0530

    Last Commit

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+    appId: "ddc3cba0-b5a8-4f71-bff9-ec5411d07a9a",
+    appPassword: "]@u2{GU4T!o-Z9PG"
 });
 
-// Listen for messages from users 
+// Listen for messages from users
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
